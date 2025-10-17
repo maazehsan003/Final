5532f3289ccbdb125a33a1c75e66e81d2bd901ec
Maaz Ehsan <maazehsan003@gmail.com>
2025-10-17 18:36:03 +0500

    okay

--- workhub/workhub/settings.py
+++ workhub/workhub/settings.py
@@ -78,11 +78,10 @@ WSGI_APPLICATION = 'workhub.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
